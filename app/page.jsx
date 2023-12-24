bbe4930f34f63722cd6f2fdef784a5749cddce4c
Sahir khan <sahir.khan@techchefz.com>
2023-12-24 14:07:36 +0530

    feat: added shadcn and converted to jsx

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center relative ">
+      <Button variant="outline">Hi</Button>
+    </div>
+  );
+}
