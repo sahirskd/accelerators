9aba83c138315f3b05ed32225f39ce06c29d4257
Sahir khan <sahir.khan@techchefz.com>
2023-12-24 13:32:48 +0530

    build: project setup

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 
+const myVar = 10;
 export default function Home() {
+  console.log(myVar)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
